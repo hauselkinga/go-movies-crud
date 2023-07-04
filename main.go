b8a16050d20dae3810a50bbde138a7e93e8f24a8
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-04 12:19:30 +0200

    create Movie and Director structs

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Fristname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
 func main() {
 	
 }
